7417a99283cfe5bdb41fd88137fbe0326a8c191c
Dongha Lee <mathicalee@gmail.com>
2018-12-06 19:14:54 +0100

    WIP Day 06 Crystal

--- 06/run1.cr
+++ 06/run1.cr
@@ -1,27 +1,40 @@
 coordinates = File.read_lines("input")
-  .map do |line|
+  .map_with_index do |line, index|
     if md = /(\d+), (\d+)/.match(line)
-      {md[1].to_i, md[2].to_i}
+      {index, md[1].to_i, md[2].to_i}
     end
   end
   .compact
 
 def boundary(coordinates, spaces = 1) # left, top, right, bottom
   {
-    coordinates.map(&.[0]).min - spaces,
-    coordinates.map(&.[1]).min + spaces,
-    coordinates.map(&.[0]).max - spaces,
-    coordinates.map(&.[1]).max + spaces,
+    coordinates.map(&.[1]).min - spaces,
+    coordinates.map(&.[2]).min + spaces,
+    coordinates.map(&.[1]).max - spaces,
+    coordinates.map(&.[2]).max + spaces,
   }
 end
 
-def distance(coordinates, x, y)
+def distance(x1, y1, x2, y2)
+  (x2 - x1).abs + (y2 - y1).abs
 end
 
 x1, y1, x2, y2 = boundary(coordinates)
-(x1..x2).map do |x|
-  (y1..y2).map do |y|
-    puts [x, y]
+result =
+  (x1..x2).flat_map do |x|
+    (y1..y2).map do |y|
+      coordinates
+        .map { |index, _x, _y| {index, distance(_x, _y, x, y)} }
+        .reduce({-1, Int32::MAX, false}) do |acc, inc|
+          if inc[1] < acc[1]
+            {inc[0], inc[1], false}
+          elsif inc[1] == acc[1]
+            {acc[0], acc[1], true}
+          else
+            acc
+          end
+        end
+    end
   end
-end
 
+puts result

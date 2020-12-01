562797b60ff8d102bac508d6213fe275bd23e7c3
Dongha Lee <dongha@0xlee.dev>
2020-12-02 00:24:19 +0100

    Refactoring, merging two runs into one

--- 2020/01/run.exs
+++ 2020/01/run.exs
@@ -1,9 +1,10 @@
 ints =
-  File.read("input1")
+  File.read("input")
   |> elem(1)
   |> String.split("\n", trim: true)
   |> Enum.map(&String.to_integer/1)
 
+# 1st part
 for(
   i <- ints,
   j <- ints,
@@ -11,4 +12,15 @@ for(
 )
 |> Enum.find(fn {x, y} -> x + y == 2020 end)
 |> (fn {x, y} -> x * y end).()
+|> IO.puts()
+
+# 2nd part
+for(
+  i <- ints,
+  j <- ints,
+  k <- ints,
+  do: {i, j, k}
+)
+|> Enum.find(fn {x, y, z} -> x + y + z == 2020 end)
+|> (fn {x, y, z} -> x * y * z end).()
 |> IO.puts()

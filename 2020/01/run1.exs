70c116ce8647b2a0a8e5fb81b01a4da065ba5f1c
Dongha Lee <dongha@0xlee.dev>
2020-12-02 00:21:18 +0100

    Refactoring and formatting

--- 2020/01/run1.exs
+++ 2020/01/run1.exs
@@ -1,9 +1,14 @@
-ints = File.read("input1") 
-     |> elem(1)
-     |> String.split("\n", trim: true)
-     |> Enum.map(&String.to_integer/1)
+ints =
+  File.read("input1")
+  |> elem(1)
+  |> String.split("\n", trim: true)
+  |> Enum.map(&String.to_integer/1)
 
-for i <- ints, j <- ints do {i, j} end 
-    |> Enum.find(fn {x, y} -> x+y == 2020 end)
-    |> (fn {x, y} -> x*y end).()
-    |> IO.puts
+for(
+  i <- ints,
+  j <- ints,
+  do: {i, j}
+)
+|> Enum.find(fn {x, y} -> x + y == 2020 end)
+|> (fn {x, y} -> x * y end).()
+|> IO.puts()

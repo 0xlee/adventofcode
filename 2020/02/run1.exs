1bfa84e28a38707ba5ac29e3cc2e5b1a7f6edb1a
Dongha Lee <dongha@0xlee.dev>
2020-12-04 02:41:06 +0100

    Day 02

---
+++ 2020/02/run1.exs
@@ -0,0 +1,16 @@
+check = fn [min, max, chr, str] ->
+  len = Enum.filter(String.split(str, "", trim: true), fn s -> s == chr end) |> length
+  len >= String.to_integer(min) and len <= String.to_integer(max)
+end
+
+count =
+  File.read("input")
+  |> elem(1)
+  |> String.split("\n", trim: true)
+  |> Enum.map(fn x ->
+    check.(tl(Regex.run(~r/([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)/, x)))
+  end)
+  |> Enum.filter(fn x -> x end)
+  |> length
+
+IO.puts count
---
+++ 2020/02/run2.exs
@@ -0,0 +1,20 @@
+check = fn [a, b, chr, str] ->
+  aa = String.to_integer(a)-1
+  bb = String.to_integer(b)-1
+  IO.puts("#{aa} #{bb} #{str} #{chr} #{String.at(str, aa) == chr}, #{String.at(str, bb) == chr}")
+  (String.at(str, aa) == chr) != (String.at(str, bb) == chr)
+  #len = Enum.filter(String.split(str, "", trim: true), fn s -> s == chr end) |> length
+  #len >= String.to_integer(min) and len <= String.to_integer(max)
+end
+
+count =
+  File.read("input")
+  |> elem(1)
+  |> String.split("\n", trim: true)
+  |> Enum.map(fn x ->
+    check.(tl(Regex.run(~r/([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)/, x)))
+  end)
+  |> Enum.filter(fn x -> x end)
+  |> length
+
+IO.puts count

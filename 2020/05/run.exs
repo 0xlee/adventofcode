f434eb61bd166f788314a057110333ee10d74b63
Dongha Lee <dongha@0xlee.dev>
2020-12-05 09:30:16 +0100

    Day 05

---
+++ 2020/05/run.exs
@@ -0,0 +1,42 @@
+use Bitwise
+
+result =
+  File.read("input")
+  |> elem(1)
+  |> String.split("\n", trim: true)
+  |> Enum.map(fn line ->
+    cs = String.split(line, "", trim: true)
+
+    Enum.reduce(cs, 0, fn x, acc ->
+      case x do
+        "F" ->
+          acc <<< 1 ||| 0
+
+        "B" ->
+          acc <<< 1 ||| 1
+
+        "L" ->
+          acc <<< 1 ||| 0
+
+        "R" ->
+          acc <<< 1 ||| 1
+      end
+    end)
+  end)
+
+# part 1
+result
+|> Enum.max
+|> IO.puts
+
+# part 2
+result
+|> Enum.sort
+|> Enum.reduce(0, fn seatId, acc ->
+	case {seatId, acc} do
+		{l, 0} -> l
+		{l, n} when l == n+1 -> l
+		{l, n} when l == n+2 -> IO.puts(n+1); l # here printing missing
+		{l, n} -> IO.puts("ERROR: #{l}, #{n}")
+	end
+end)
